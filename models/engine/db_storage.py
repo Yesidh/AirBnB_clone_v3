0013f5900f1d76d4363f929eb1ccaede0d1d4ee0
Yesidh <944@holbertonschool.com>
2020-01-28 18:16:19 -0500

    [Feature]: adding the methods get and count in each file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,17 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Method to retrieve one object"""
+
+        key = cls + '.' + id
+        _list = self.all(cls)
+        return _list[key]
+
+    def count(self, cls=None):
+        """Method to count the number of objects in storage"""
+
+        if cls is None:
+            return len(self.all())
+        return len(self.all(cls))
