d0e29de6f84265961fd7b3cec720260e9f101481
Yesidh <944@holbertonschool.com>
2020-01-29 22:06:22 -0500

    [Feature]: adding state.py for use GET, POST, PUT and DELETE methods, importing states within __init__.py

---
+++ api/v1/views/states.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+===========================================================
+Retrieve the list of all State objects: GET /api/v1/states
+Retrieve a State object: GET /api/v1/states/<state_id>
+Delete State object: DELETE /api/v1/staes/<state_id>
+Creates a State: POST /api/v1/states
+Updates a State: PUT /api/v1/states/<state_id>
+===========================================================
+"""
+
+from api.v1.views import app_views
+from models import storage
+from flask import jsonify, abort, request, make_response
+from models.state import State
+
+@app_views.route('/states', strict_slashes=False)
+def retrieve_states():
+    """ Retrieve the list of all State objects"""
+
+    state_list = []
+    for key, value in storage.all("State").items():
+        state_list.append(value.to_dict())
+    return jsonify(state_list)
+
+@app_views.route('/states/<string:state_id>', strict_slashes=False)
+def states_id(state_id):
+    """Method to retrieve an state using the id"""
+
+    key = 'State.' + state_id
+    if key in storage.all("State").keys():
+        return jsonify(storage.all("State").get(key).to_dict())
+    else:
+        abort(404)
+
+@app_views.route('/states/<string:state_id>', methods=['DELETE'], strict_slashes=False)
+def del_states_id(state_id):
+    """Method to delete an state using the id"""
+
+    key = 'State.' + state_id
+    if key in storage.all("State").keys():
+        obj = storage.get("State", state_id)
+        storage.delete(obj)
+        storage.save()
+        return jsonify({})
+    else:
+        abort(404)
+
+@app_views.route('/states/', methods=['POST'], strict_slashes=False)
+def post_state():
+    """Method to creates an State object using POST"""
+
+    if not request.get_json():
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    if not 'name' in request.get_json():
+        return make_response(jsonify({"error": "Missing name"}), 400)
+    obj = State(**request.get_json())
+    obj.save()
+    return make_response(obj.to_dict(), 201)
+
+@app_views.route('/states/', methods=['PUT'], strict_slashes=False)
+def put_state():
+    """Method to creates an State object using POST"""
+
+    pass
