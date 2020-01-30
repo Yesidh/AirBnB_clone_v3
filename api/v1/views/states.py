ed1c6f381b1e8457a33f867d2c34297948630956
sechchr22 <980@holbertonschool.com>
2020-01-30 22:53:05 +0000

    removed else from POST method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -85,7 +85,7 @@ def put_state(state_id):
             for k, v in obj2.items():
                 if key == k:
                     obj[key] = v
-                else:
-                    pass
+                'else:
+                    pass'
     storage.save()
     return make_response(obj, 200)
